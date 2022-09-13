845094ad6f455e0ad531adbe9df3a961e44b83f5
Guilherme Sanita <guilherme.ssanita@gmail.com>
2022-09-13 17:14:08 -0300

    Ex 2 - Usando e (&&)

---
+++ Condicionais/Ex.2.js
@@ -0,0 +1,11 @@
+const temIngresso = true
+const censura = 16
+const idade = 19
+
+// Para entrar: Ter ingresso e Idade igual ou maior que Censura
+
+if (temIngresso === true && idade >= censura) {
+    console.log("Pode entrar");
+} else {
+    console.log("Barrado");
+}
