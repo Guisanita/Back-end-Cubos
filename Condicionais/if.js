ead3b1db40b1c1540ddb05c016206aa6be3dc796
Guilherme Sanita <guilherme.ssanita@gmail.com>
2022-09-13 16:33:20 -0300

    Colocando else

--- Condicionais/if.js
+++ Condicionais/if.js
@@ -1,8 +1,11 @@
-const idade = 30
+const idade = 3
 
 if (idade >= 18) {
     // Aqui fica o que roda apenas se a boolean for True
     console.log("É maior de idade");
+} else {
+    // Aqui fica o que rodar se a boolean for False
+    console.log("É menor de idade");
 }
 
 console.log("Fim do código");
