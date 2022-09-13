0d85aef1bf10e0b95e0b3e52a423f11ee14b482e
Guilherme Sanita <guilherme.ssanita@gmail.com>
2022-09-13 16:50:23 -0300

    Testando if else

---
+++ Condicionais/if-else.js
@@ -0,0 +1,9 @@
+const idade = 65
+// Menor de Idade (-18), Adulto (18 a 65 anos) e idoso (mais de 65 anos)
+if (idade < 18) {
+    console.log("Menor de Idade");
+} else if (idade <= 65) {
+    console.log("Adulto");
+} else {
+    console.log("Idoso");
+}
