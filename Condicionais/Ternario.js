1e59131e2987ef0873a340b55affc3b9008ceeba
Guilherme Sanita <guilherme.ssanita@gmail.com>
2022-09-13 21:02:22 -0300

    Resolvendo da minha forma

--- Condicionais/Ternario.js
+++ Condicionais/Ternario.js
@@ -1,10 +1,10 @@
-const saldo = 0
+const saldo = 6
 
 if (saldo > 1) {
     console.log(`Seu saldo é ${saldo} reais.`);
-} else if (saldo = 0) {
+} else if (saldo === 0) {
     console.log("Você não tem saldo.");
-} else if (saldo = 1) {
+} else if (saldo === 1) {
     console.log("Seu saldo é 1 real");
 } else if (saldo < 0) {
     console.log("Seu saldo é negativo");
