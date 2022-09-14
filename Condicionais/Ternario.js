cf55159c3cf0ee0f78f6b8798e82174e68db203f
Guilherme Sanita <guilherme.ssanita@gmail.com>
2022-09-13 21:00:32 -0300

    Erro, usando o = ao inves de ===

---
+++ Condicionais/Ternario.js
@@ -0,0 +1,13 @@
+const saldo = 0
+
+if (saldo > 1) {
+    console.log(`Seu saldo é ${saldo} reais.`);
+} else if (saldo = 0) {
+    console.log("Você não tem saldo.");
+} else if (saldo = 1) {
+    console.log("Seu saldo é 1 real");
+} else if (saldo < 0) {
+    console.log("Seu saldo é negativo");
+} else {
+    console.log(`Seu saldo é ${saldo} centavos`);
+}
