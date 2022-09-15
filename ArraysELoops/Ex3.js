24a79b1ed616e1693a70546e9e1cf0d5431b59e7
Guilherme Sanita <guilherme.ssanita@gmail.com>
2022-09-15 18:53:17 -0300

    Pensando na solução

---
+++ ArraysELoops/Ex3.js
@@ -0,0 +1,5 @@
+const numerosAleatorios = [2, 3, 5, 6, 7, 9];
+
+let soma = numerosAleatorios[0] + numerosAleatorios[1] + numerosAleatorios[2] + numerosAleatorios[3] + numerosAleatorios[4] + numerosAleatorios[5];
+
+console.log(soma);
