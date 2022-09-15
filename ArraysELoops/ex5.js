7f8ef251248e8a1cd402ebf78efce38ab5766b0c
Guilherme Sanita <guilherme.ssanita@gmail.com>
2022-09-15 20:47:46 -0300

    Resolução professor

--- ArraysELoops/ex5.js
+++ ArraysELoops/ex5.js
@@ -1,6 +1,11 @@
-let contagem = 10
+let contagem = 10;
 
 while (contagem >= 0) {
     console.log(contagem);
     contagem = contagem - 1;
 }
+
+/* Resolução Professor com for
+for (let contagem = 10; contagem >= 0; contagem--) {
+    console.log(contagem);
+} */
