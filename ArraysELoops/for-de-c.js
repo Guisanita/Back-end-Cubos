209409b1c062a267e872aa6f639958e44a2ae743
Guilherme Sanita <guilherme.ssanita@gmail.com>
2022-09-15 17:10:48 -0300

    Usando for

---
+++ ArraysELoops/for-de-c.js
@@ -0,0 +1,16 @@
+const nomes = ["Joao", "Maria", "Osvaldo", "Arthur", "Valeriana"];
+
+for (let i = 0; i < nomes.length; i++) {
+    console.log(nomes[i]);
+}
+
+
+
+
+/*let x = 0
+
+while (x < nomes.length) {
+    console.log(nomes[x]);
+    x++
+} */
+
