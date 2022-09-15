8f4b41ee1e434a661d0a757e949c0c17144c37b4
Guilherme Sanita <guilherme.ssanita@gmail.com>
2022-09-15 16:48:19 -0300

    Repetindo código para entender o while

--- ArraysELoops/while.js
+++ ArraysELoops/while.js
@@ -1,7 +1,13 @@
 const nomes = ["Joao", "Maria", "Osvaldo", "Arthur", "Valeriana"];
 
-console.log(nomes[0]);
-console.log(nomes[1]);
-console.log(nomes[2]);
-console.log(nomes[3]);
-console.log(nomes[4]);
+let x = 0
+
+console.log(nomes[x]);
+x++
+console.log(nomes[x]);
+x++
+console.log(nomes[x]);
+x++
+console.log(nomes[x]);
+x++
+console.log(nomes[x]);
