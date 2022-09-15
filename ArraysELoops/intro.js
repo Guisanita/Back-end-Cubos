c73397ad4bb360d077b5315592456bed4f044987
Guilherme Sanita <guilherme.ssanita@gmail.com>
2022-09-15 15:00:29 -0300

    Testando introdução de arrays e Loops

---
+++ ArraysELoops/intro.js
@@ -0,0 +1,16 @@
+const nome1 = "João";
+const nome2 = "Maria";
+const nome3 = "Joana";
+
+const nomesEstudantes = ["João", "Maria", "Joana"];
+const notasEstudantes = [2, 4, 10];
+
+console.log(nomesEstudantes);
+console.log(nomesEstudantes[0]);
+console.log(nomesEstudantes[1]);
+console.log(nomesEstudantes[2]);
+console.log(nomesEstudantes[3]);
+console.log(notasEstudantes);
+console.log(notasEstudantes[0]);
+console.log(notasEstudantes[1]);
+console.log(notasEstudantes[2]);
