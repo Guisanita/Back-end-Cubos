9367da0321ea2fc700c46c5d24ccde7d9de06366
Guilherme Sanita <guilherme.ssanita@gmail.com>
2022-09-15 22:50:51 -0300

    Pode colocar o to Fixed na formula direto, se não for utilizar pra outras contas, tudo depende

--- Condicionais/Exercicio1.js
+++ Condicionais/Exercicio1.js
@@ -23,7 +23,7 @@ if (numeroParcelas === 1) {
     let valorComDesconto = valorCompra - desconto;
     console.log(`Como você pagará à vista, o valor do produto é ${valorComDesconto}.`);
 } else {
-    let parcela = valorCompra / numeroParcelas;
-    console.log(`Como você pagará em ${numeroParcelas} vezes, o valor de cada parcela é ${parcela.toFixed(2)}.`);
+    let parcela = (valorCompra / numeroParcelas).toFixed(2);
+    console.log(`Como você pagará em ${numeroParcelas} vezes, o valor de cada parcela é ${parcela}.`);
 }
 
