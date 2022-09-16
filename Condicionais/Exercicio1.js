abd23a6f01d634b88fb1efa1bcdcdac08de5b262
Guilherme Sanita <guilherme.ssanita@gmail.com>
2022-09-15 22:37:05 -0300

    Com resolução do Professor

--- Condicionais/Exercicio1.js
+++ Condicionais/Exercicio1.js
@@ -1,9 +1,18 @@
 const valorCompra = 100;
-const numeroParcelas = 4;
+const numeroParcelas = 1;
 
 if (numeroParcelas === 1) {
     let vista = valorCompra * 0.9;
     console.log(`Como você pagará à vista, o valor do produto é ${vista}.`);
+} else {
+    let parcela = valorCompra / numeroParcelas;
+    console.log(`Como você pagará em ${numeroParcelas} vezes, o valor de cada parcela é ${parcela}.`);
+}
+
+if (numeroParcelas === 1) {
+    let desconto = valorCompra * 0.1;
+    let valorComDesconto = valorCompra - desconto;
+    console.log(`Como você pagará à vista, o valor do produto é ${valorComDesconto}.`);
 } else {
     let parcela = valorCompra / numeroParcelas;
     console.log(`Como você pagará em ${numeroParcelas} vezes, o valor de cada parcela é ${parcela}.`);
