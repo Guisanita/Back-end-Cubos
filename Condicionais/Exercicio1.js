99299b002360d863982f15b7666f86cb0040b67b
Guilherme Sanita <guilherme.ssanita@gmail.com>
2022-09-15 22:47:16 -0300

    Testando agora usando o toFixed

--- Condicionais/Exercicio1.js
+++ Condicionais/Exercicio1.js
@@ -1,5 +1,5 @@
 const valorCompra = 100;
-const numeroParcelas = 1;
+const numeroParcelas = 3;
 
 if (numeroParcelas === 1) {
     let vista = valorCompra * 0.9;
@@ -17,3 +17,13 @@ if (numeroParcelas === 1) {
     let parcela = valorCompra / numeroParcelas;
     console.log(`Como você pagará em ${numeroParcelas} vezes, o valor de cada parcela é ${parcela}.`);
 }
+
+if (numeroParcelas === 1) {
+    let desconto = valorCompra * 0.1;
+    let valorComDesconto = valorCompra - desconto;
+    console.log(`Como você pagará à vista, o valor do produto é ${valorComDesconto}.`);
+} else {
+    let parcela = valorCompra / numeroParcelas;
+    console.log(`Como você pagará em ${numeroParcelas} vezes, o valor de cada parcela é ${parcela.toFixed()}.`);
+}
+
