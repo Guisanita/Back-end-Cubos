44fd321ff3fea2c1c7ea8154c55702c75d134c3f
Guilherme Sanita <guilherme.ssanita@gmail.com>
2022-09-23 20:44:55 -0300

    Minha resolução

---
+++ Objetos/Ex3.js
@@ -0,0 +1,30 @@
+const produtosConsumidos = [
+    {
+        nomeProduto: "Cerveja",
+        precoUnitario: 8500,
+        quantidadeProduto: 4
+    },
+    {
+        nomeProduto: "Agua",
+        precoUnitario: 500,
+        quantidadeProduto: 1
+    },
+    {
+        nomeProduto: "Espeto",
+        precoUnitario: 700,
+        quantidadeProduto: 2
+    }
+];
+
+const cliente = {
+    nome: "Guilherme",
+    idade: 30,
+    produto: produtosConsumidos
+};
+
+console.log(cliente.nome);
+console.log(cliente.idade);
+cliente.idade = 45;
+console.log(cliente.idade);
+console.log(cliente.produto[0].nomeProduto);
+console.log(cliente.produto[2].precoUnitario);
