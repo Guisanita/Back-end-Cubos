ea112247c39c04cad2df196845149b29027a4768
Guilherme Sanita <guilherme.ssanita@gmail.com>
2022-09-23 20:08:53 -0300

    4

---
+++ Objetos/shorthand.js
@@ -0,0 +1,13 @@
+const pessoa = {
+    nome: "Guilherme",
+    altura: 1.65,
+    peso: 72,
+    carro: {
+        marca: "Toyota",
+        modelo: "Etios",
+        ano: 2020,
+        potencia: 80
+    }
+};
+
+console.log(pessoa.carro.marca);
