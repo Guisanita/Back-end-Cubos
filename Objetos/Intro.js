f651b9b4d9ab4218a1efa390f8d4695aad7392b0
Guilherme Sanita <guilherme.ssanita@gmail.com>
2022-09-23 18:52:06 -0300

    1

---
+++ Objetos/Intro.js
@@ -0,0 +1,10 @@
+const pessoa = {
+    nome: "Guilherme",
+    altura: 1.65,
+    peso: 72
+};
+
+pessoa.nome = "Gui";
+
+console.log(`Meu nome é ${pessoa.nome}, tenho ${pessoa.altura} metros e peso ${pessoa.peso} Kg.`);
+
