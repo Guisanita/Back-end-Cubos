0b57bf250cc4fe9c1e423cc116edb6ce819d4f4e
Guilherme Sanita <guilherme.ssanita@gmail.com>
2022-09-27 16:40:40 -0300

    Resolução professor

--- Funcoes/Ex3.js
+++ Funcoes/Ex3.js
@@ -1,18 +1,34 @@
-const pessoa = {
+function apresentacao(pessoa) {
+    if (pessoa.idade < 25) {
+        console.log(`Sou ${pessoa.nome}, sou um(a) jovem de ${pessoa.idade} anos e sou ${pessoa.profissao}.`);
+    }
+    else if (pessoa.idade < 65) {
+        console.log(`Sou ${pessoa.nome}, sou um(a) adulto(a) de ${pessoa.idade} anos e sou ${pessoa.profissao}.`);
+    } else {
+        console.log(`Sou ${pessoa.nome}, sou um(a) idoso(a) de ${pessoa.idade} anos e sou ${pessoa.profissao}.`);
+    }
+}
+
+const pessoa1 = {
     nome: "José",
     idade: 30,
     profissao: "professor"
 }
 
-function apresentacao(pessoa) {
-    if (pessoa.idade < 25) {
-        return `Sou ${pessoa.nome}, sou um(a) jovem de ${pessoa.idade} anos e sou ${pessoa.profissao}.`
-    }
-    else if (pessoa.idade < 65) {
-        return `Sou ${pessoa.nome}, sou um(a) adulto(a) de ${pessoa.idade} anos e sou ${pessoa.profissao}.`
-    } else {
-        return `Sou ${pessoa.nome}, sou um(a) idoso(a) de ${pessoa.idade} anos e sou ${pessoa.profissao}.`
-    }
+apresentacao(pessoa1);
+
+const pessoa2 = {
+    nome: "Ana",
+    idade: 40,
+    profissao: "Advogada"
 }
 
-console.log(apresentacao(pessoa));
+apresentacao(pessoa2);
+
+const pessoa3 = {
+    nome: "João",
+    idade: 12,
+    profissao: "estudante"
+}
+
+apresentacao(pessoa3);
