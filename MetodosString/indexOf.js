aa8714ba824b34df2aa4772558e88d8c7a39b5c5
Guilherme Sanita <guilherme.ssanita@gmail.com>
2022-09-29 15:29:56 -0300

    Resolvido pelo professor e entendido

--- MetodosString/indexOf.js
+++ MetodosString/indexOf.js
@@ -1,11 +1,23 @@
-const email = "teste@teste.com";
+// const email = "teste@teste.com";
 
-if (email.includes("@")) {
-    if (email.indexOf(".", email.indexOf("@")) = -1) {
-        console.log("Email inválido.")
-    } else {
-        console.log("Email válido.")
-    }
+// if (email.includes("@")) {
+//     if (email.indexOf(".", email.indexOf("@")) = -1) {
+//         console.log("Email inválido.")
+//     } else {
+//         console.log("Email válido.")
+//     }
+// } else {
+//     console.log("Email inválido.")
+// }
+
+const possivelEmail = "teste.teste@teste.com";
+
+const indexArroba = possivelEmail.indexOf("@");
+const indexPontoDepoisArroba = possivelEmail.indexOf(".", indexArroba);
+
+if (indexPontoDepoisArroba > indexArroba) {
+    console.log("atende as requisições");
 } else {
-    console.log("Email inválido.")
+    console.log("Não há ponto depois do Arroba.");
 }
+
