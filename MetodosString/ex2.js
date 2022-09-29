9cf9ca0b12fd80ef67ccd854bf6b18882512787f
Guilherme Sanita <guilherme.ssanita@gmail.com>
2022-09-29 16:40:03 -0300

    Minha resolução, não deu certo

---
+++ MetodosString/ex2.js
@@ -0,0 +1,13 @@
+const numeroComVirgula = "1,350,000";
+
+let numeroComPonto = numeroComVirgula.replace(",", ".");
+
+for (let i = 0; i < numeroComVirgula.length; i++) {
+    numeroComPonto
+}
+
+
+console.log(numeroComPonto);
+console.log(numeroComVirgula);
+
+
