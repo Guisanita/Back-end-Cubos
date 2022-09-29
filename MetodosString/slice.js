e3386cc04e342f90215442c9d822903898b338a8
Guilherme Sanita <guilherme.ssanita@gmail.com>
2022-09-29 15:51:28 -0300

    Resolução Professor

--- MetodosString/slice.js
+++ MetodosString/slice.js
@@ -1,5 +1,10 @@
 const cidade = "Salvador-BA";
 
-let estado = cidade.slice(-2);
+// let estado = cidade.slice(-2);
+
+// console.log(estado);
+
+const penultimoIndex = cidade.length - 2
+let estado = cidade.slice(penultimoIndex);
 
 console.log(estado);
