e3ab299045d4d5f1fe2f8ad9859bade2548bf81c
Guilherme Sanita <guilherme.ssanita@gmail.com>
2022-11-03 16:41:45 -0300

    Aprendendo exportar e importar arquivos

---
+++ PrimeiroServidor/array.js
@@ -0,0 +1,7 @@
+const arrayNumeros = [1, 2, 4, 4, 5, 6, 4, 7];
+const arrayLetras = ["a", "g", "a", "g", "h", "i"];
+
+module.exports = {
+    arrayNumeros,
+    arrayLetras
+};
--- PrimeiroServidor/index.js
+++ PrimeiroServidor/index.js
@@ -1,9 +1,18 @@
 // const lodash = require("lodash");
 const { uniq } = require("lodash")
+const { arrayNumeros, arrayLetras } = require("./array");
 
-const array = [1, 2, 4, 4, 5, 6, 4, 7];
+
+// const array = [1, 2, 4, 4, 5, 6, 4, 7];
+
 
 // const arrayUnico = lodash.uniq(array);
-const arrayUnico = uniq(array);
+
+
+// const arrayUnico = uniq(array);
+// const arrayUnico = uniq(array.arrayNumeros)
+const arrayUnico = uniq(arrayNumeros)
+
+
 
 console.log(arrayUnico);
