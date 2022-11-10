31c4cce11cfd5669d08989448c5576dbbc6e2016
Guilherme Sanita <guilherme.ssanita@gmail.com>
2022-11-09 21:00:03 -0300

    Ex2ResoluçãoProf

--- RotasIntermediariosControladores/src/middlewares/cars.js
+++ RotasIntermediariosControladores/src/middlewares/cars.js
@@ -1,11 +1,25 @@
+//Minha resolução
+// const password = (req, res, next) => {
+//     const { senha } = req.query;
+
+//     if (senha === "carros123") {
+//         next();
+//     } else {
+//         return res.send("Acesso negado, senha incorreta");
+//     }
+// }
+
+//Resolução Professor
 const password = (req, res, next) => {
     const { senha } = req.query;
 
-    if (senha === "carros123") {
-        next();
-    } else {
+    if (!senha) {
+        return res.send("A senha não foi informada");
+    } else if (senha !== "carros123") {
         return res.send("Acesso negado, senha incorreta");
     }
+
+    next();
 }
 
 module.exports = password;
